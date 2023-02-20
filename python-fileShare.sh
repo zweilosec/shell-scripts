92b844bd532a144b5ba6af510fbdbff9b624bdec
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2023-02-20 14:55:38 -0600

    update share script to optionally take arguments
    Add -p $port and -d $directory arguments to allow for better flexibility

--- python-fileShare.sh
+++ python-fileShare.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
+# Script for easily sharing files through http, with easy to copy links provided
+# Helper functions and optional argument info from http://mywiki.wooledge.org/BashFAQ/035?action=print
 
-#Makes different colored text
+#Define ANSI codes for colored text
 GN="\e[32m"
 RES="\e[0m"
 CYAN="\e[1;36m"
 
-#font=Big http://www.patorjk.com/software/taag/
-echo -e "\n$CYAN""
-  _____       _   _                   ______ _ _       _____                          
- |  __ \     | | | |                 |  ____(_) |     / ____|                         
- | |__) |   _| |_| |__   ___  _ __   | |__   _| | ___| (___   ___ _ ____   _____ _ __ 
- |  ___/ | | | __| '_ \ / _ \| '_ \  |  __| | | |/ _ \ ___ \ / _ \ '__\ \ / / _ \ '__|
- | |   | |_| | |_| | | | (_) | | | | | |    | | |  __/____) |  __/ |   \ V /  __/ |   
- |_|    \__, |\__|_| |_|\___/|_| |_| |_|    |_|_|\___|_____/ \___|_|    \_/ \___|_|   
-         __/ |                                                                        
-        |___/                                                                         
-$RES"
-echo -e "Created By$GN Ac1d $RES\n"
-echo -e "Updated by$CYAN zweilos $RES\n"
+echo -e "Created By$GN Ac1d $RES"
+echo -e "Updated by$CYAN zweilosec$RES 20FEB2023\n"
+
+# For printing usage info
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-h] [-d DIRECTORY] [-p PORT]
+    Share the contents of a directory over HTTP using
+    python3 -m http.server.
+
+    -h            display this help and exit
+    -d DIRECTORY  directory to share files from
+    -p            port to host the server on
+EOF
+}
+
+die() {
+    printf '%s\n' "$1" >&2
+    exit 1
+}
+
+# Initialize all the option variables.
+# This ensures we are not contaminated by variables from the environment.
+port=8099
+directory="."
+
+while :; do
+    case $1 in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        -d|--dir)
+            if [[ -d $2 ]]; then
+                directory="$2"
+                shift
+            else
+                die 'ERROR: $2 is not a directory!'
+            fi
+            ;;
+        -p|--port)
+            port=$2
+            shift
+            ;;
+        *)
+            break
+    esac
+
+    shift
+done
 
 #list IPs associated with current hostname
 HN="hostname -I"
@@ -25,8 +64,6 @@ HN="hostname -I"
 res=$(eval $HN)
 arrIN=(${res// / })
 IP=""
-#TODO:Make GetPort function that takes either user input or cmdline argument for custom port
-PORT=8099
 
 #if there is more than one IP available, list the first two as options
 #TODO: make a way to list all options
@@ -48,33 +85,32 @@ if [ ${#arrIN[@]} -gt 1 ]; then
                 ;;
                 #Selecting "3" will exit the program
                 "Quit")
-                exit 0
+                break
                 ;;
-                
                 #Any input other than 1-3 will result in this error message
-                *) echo "Invalid option: $REPLY"
-                ;;
+                *) echo "Invalid option: $REPLY";;
         esac
         done
 else
-       IP=$arrIN
+    IP=$arrIN
 
 fi
 echo ""
 echo "IP: "$IP
 echo ""
 echo -e "Download files using these links:\n"
-for entry in `ls`;do
+# For each file in the shared directory, create a wget link
+for entry in `ls $directory`;do
         if  [  ! -d $entry  ];then
-                wgetCmd=$(echo "wget http://${IP##*( )}:8099/$entry" | xargs)
+                wgetCmd=$(echo "wget http://${IP##*( )}:$port/$entry" | xargs)
                 echo -e "\t$GN$wgetCmd$RES"
         fi
 done
 echo ""
-echo -e "\nCurrent Directory Contents:"
-ls --color .
+echo -e "\nDirectory Contents of $directory"
+ls --color $directory
 echo ""
-echo -e "\nStarting Server..."
+echo -e "\nStarting Server"
 
-#Opens HTTP Server in the folder the command is run from on port $PORT
-python3 -m http.server $PORT -d .
+#Opens HTTP file server from the specified $directory on port $port
+python3 -m http.server $port -d $directory
