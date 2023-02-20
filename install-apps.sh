c8c7c1cb5f4126e9f5388d478f83a6f38aeb0629
WolfZweiler <61819873+zweilosec@users.noreply.github.com>
2023-02-20 15:01:39 -0600

    add strong warning against breaking your distro

--- install-apps.sh
+++ install-apps.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #Intended to be used on non-Kali (Debian) systems if you need access to specific tools
+#
+# WARNING! - This is dangerous if you install dependencies that overwrite the ones for your specific distro
+# make sure to only install programs that minimize dependencies, and disable this source when not
+# deliberately installing a program.  WILL definitely break your distro if you run apt upgrade blindly
 
 #---------------------------------------------------------------------------------------------------
 #  Add Kali repository to /etc/apt/sources.list                                                   /
@@ -13,15 +17,14 @@ echo deb http://http.kali.org/kali kali-rolling main non-free contrib >> /etc/ap
 #--------------------------------------------------------------------------------------------------
 
 apt update -y
-apt upgrade -y
+#apt upgrade -y  <-- DO NOT do this!!
 
 #-------------------------------------------------------------------------------------------------
-#  Install apps - modify this list as needed                                                                                  /
+#  Install kali-specific programs - modify this list as needed                                                                                  /
 #-------------------------------------------------------------------------------------------------
 
 apt install -y \
   terminator \
-  ftp \
   exiftool \
   hurl \
   veil \
